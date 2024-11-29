fd49ea7f9c2c4cffce194fe6e1296431ef939fdb
CanYouBunnyHop <greenplus000@protonmail.com>
2024-11-30 07:59:57 +0800

    Update index.js

--- Debug/index.js
+++ Debug/index.js
@@ -2,8 +2,8 @@ import Vector2 from "../modules/Vector2.js";
 import { Card, getSuitColor, getNeigbourRanks, CARD_DATA } from "../modules/PlayingCards.js";
 import { setAllElementWithLogic, popRandomFromArr, getCSSDeclaredValue, convertCSSPropertyToNumeric } from "../modules/MyMiscUtil.js";
 import { requestFrame, timer } from "../modules/CSSAnimationUtil.js";
-import { startDrag, slotLogic } from "../modules/MyDraggables.js";
-import { Memento, Caretaker } from "../modules/UndoPattern.js";
+//import { startDrag, slotLogic } from "../modules/MyDraggables.js";
+//import { Memento, Caretaker } from "../modules/UndoPattern.js";
 
 const divs = document.body.querySelectorAll('div');
 divs[0].innerHTML = `
